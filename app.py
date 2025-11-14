280c97119ff99188ba2dfeb57e84b3c0051aeb53
Paul kimani <pmushiri@kabarak.ac.ke>
2025-11-14 15:03:37 +0300

    modified ports

--- app.py
+++ app.py
@@ -302,6 +302,12 @@ def init_db():
         print("✓ Database initialized successfully!")
 
 
-if __name__ == '__main__':
-    init_db()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     init_db()
+#     app.run(debug=True, host='0.0.0.0', port=5000)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
